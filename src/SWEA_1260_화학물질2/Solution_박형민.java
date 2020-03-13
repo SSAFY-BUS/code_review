60708ade3af3ce995c05df503447295c69a6b7a1
박형민 (Park Hyeoung Min) <o_ogog@naver.com>
2020-03-13 21:20:55 +0900

    Update Solution_박형민.java

--- src/SWEA_1260_화학물질2/Solution_박형민.java
+++ src/SWEA_1260_화학물질2/Solution_박형민.java
@@ -2,7 +2,7 @@ package SWEA_1260_화학물질2;
 /**
 적용 알고리즘
 	DP(Memoization), Greedy, HashSet
-	HashSet을 이용하여 첫번째 행렬을 찾은 뒤 가능한 행렬의 경우를 하나로 추려서 완전 탐색을 하되, (Greedy)
+	Hash을 이용하여 첫번째 행렬을 찾은 뒤 가능한 행렬의 경우를 하나로 추려서 완전 탐색을 하되, (Greedy)
 	Memoization을 통해 평균 시간 복잡도를 줄임.
 	
 메모리
